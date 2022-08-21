38274f824e23c8212e0b9d5112f4ef1e566db59b
dmadon <deanna.madon@gmail.com>
2022-08-20 20:57:47 -0500

    fixed syntax in connection

--- config/connection.js
+++ config/connection.js
@@ -9,13 +9,15 @@ if(process.env.JAWSDB_URL){
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 // when we are using our local maching, use our local database:
-else(sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+else{
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-  })
+  });
+}
 
-)
+
 
 
 
