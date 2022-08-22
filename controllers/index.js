1c9eec6f00fbfccf4e2e54c555ad5e79c99a6714
dmadon <deanna.madon@gmail.com>
2022-08-22 00:09:53 -0500

    create homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 router.use('/api',apiRoutes);
+router.use('/',homeRoutes) ;
 
 // This is so if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource:
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT||3001;
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars',hbs.engine);
+app.set('view engine','handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')));
 
 // turn on routes
 app.use(routes);
@@ -13,7 +20,7 @@ app.use(routes);
 // TURN ON CONNECTION TO DATABASE SERVER:
 // note: turning the "force" parameter to "true" forces the database connection to sync with model definitions and associations. 
 // by forcing the sync method to true, we will make the tables re-create if there are any association changes. performs similarly to "DROP TABLE IF EXISTS".
-// keep this parameter set to "false" until you make new associations, then after you restart the server, change it back to "false". otherwise,
+// keep this parameter set to "false" until you make new associations, then after you restart the server with the parameter set to "true", change it back to "false". otherwise,
 // your server will drop all your tables and recreate them every time it starts and you will lose all your data that you've entered into them!
 sequelize.sync({force: false})
     .then(() => {
