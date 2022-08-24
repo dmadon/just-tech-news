901d0d63e5dbafb3b06677ef4026a7a361ce1df0
dmadon <deanna.madon@gmail.com>
2022-08-23 23:29:48 -0500

    add helper functions to handlebars

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT||3001;
 const path = require('path');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
     secret:'ClassifiedTopSecret',
